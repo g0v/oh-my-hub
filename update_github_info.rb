2688ad02245eb6fb21d81327b2cbf26a32dc5a7a
Chien Kuo <chien.cc.kuo@gmail.com>
2014-11-23 01:49:13 -0500

    adds github issue count and timestamp

--- update_github_info.rb
+++ update_github_info.rb
@@ -3,23 +3,25 @@ require 'pry'
 require 'github_api'
 
 base_uri = 'https://oh-my-hub.firebaseio.com/'
-
 firebase = Firebase::Client.new(base_uri)
 
 response = firebase.get('projects')
 response.body.each do |key, element|
-
   next unless element['workspace']
-  github_workspace = element['workspace'].detect{ |ws| ws['url'].include?('github.com') }
-  github_url = github_workspace['url']
-  url_split = github_url.split('/')
-  repo_name = url_split[-1]
-  user_name = url_split[-2]
+  begin
+    github_workspace = element['workspace'].detect{ |ws| ws['url'].include?('github.com') }
+    github_url = github_workspace['url']
+    url_split = github_url.split('/')
+    repo_name = url_split[-1]
+    user_name = url_split[-2]
 
-  response = Github.repos.get(user_name, repo_name)
-  if response.status == 200
-    payload = { created_at: response.body['created_at'], updated_at: response.body['updated_at'] }
-    firebase.update("projects/#{key}", payload)
-    print "."
+    response = Github.repos.get(user_name, repo_name)
+    if response.status == 200
+      payload = { created_at: response.body['created_at'], updated_at: response.body['updated_at'], open_issues_count: response['open_issues_count'] }
+      firebase.update("projects/#{key}", payload)
+      print "."
+    end
+  rescue
+    print "E"
   end
 end
